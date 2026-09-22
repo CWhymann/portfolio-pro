645f64354a98ecb28d61fa8169c2d040420e9478
Christian <cwiemann39@gmial.com>
2026-09-22 20:58:29 +0200

    feat: add contact section

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Translation } from '../../core/translation';
 
 @Component({
   imports: [],
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss',
   templateUrl: './contact.html',
 })
-export class Contact {}
+export class Contact {
+  constructor(public translation: Translation) {}
+}
